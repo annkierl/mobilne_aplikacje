89142727cd907f2a5c0ebeee29141caca53d6255
Michal [The Fox] Dyro <mdyro@atlassian.com>
2019-02-15 00:55:17 +0100

    Fix degree count. Merge camera with compas

--- MyCompass2/app/src/main/java/com/example/mycompass2/MainActivity.java
+++ MyCompass2/app/src/main/java/com/example/mycompass2/MainActivity.java
@@ -1,42 +1,53 @@
 package com.example.mycompass2;
 
 import android.content.Context;
+import android.hardware.Camera;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
-import android.widget.ImageView;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     TextView tv_mdegree;
-    ImageView iv_marrow;
-    private static SensorManager msensorManager;
-    private Sensor mSensor;
-    private float currentDegree;
+    private static SensorManager mSensorManager;
+    Camera camera;
+    FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv_mdegree = findViewById(R.id.degree);
-        iv_marrow =findViewById(R.id.iv_arrow);
-        msensorManager =(SensorManager)getSystemService(Context.SENSOR_SERVICE);
-        mSensor=msensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-    }
+        frameLayout = findViewById(R.id.camera);
 
+        camera = Camera.open();
+        ShowCamera showCamera;
+        showCamera = new ShowCamera(this, camera);
+        frameLayout.addView(showCamera);
+
+
+        mSensorManager =(SensorManager)getSystemService(Context.SENSOR_SERVICE);
+
+        mSensorManager.registerListener(this, mSensorManager
+                        .getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
+                SensorManager.SENSOR_DELAY_UI);
+
+        mSensorManager.registerListener(this, mSensorManager
+                        .getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_UI);
+    }
 
 
     @Override
     protected void onResume() {
         super.onResume();
         if(mSensor!=null){
-            msensorManager.registerListener(this,mSensor,SensorManager.SENSOR_DELAY_FASTEST);
+            mSensorManager.registerListener(this,mSensor,SensorManager.SENSOR_DELAY_FASTEST);
         }
         else{
             Toast.makeText(MainActivity.this,"Sensor not supported!",Toast.LENGTH_SHORT);
@@ -47,20 +58,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     protected void onPause() {
         super.onPause();
-        msensorManager.unregisterListener(this);
+        mSensorManager.unregisterListener(this);
     }
+
+    float [] mGravity;
+    float [] mGeomagnetic;
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-    int degree=Math.round(event.values[0]);
-    tv_mdegree.setText(Integer.toString(degree)+(char)0*0030);
-        RotateAnimation rotateAnimation= new RotateAnimation(currentDegree,-degree, Animation.RELATIVE_TO_SELF,0.5f,
-                Animation.RELATIVE_TO_SELF,0.5f);
-        rotateAnimation.setDuration(500);
-        rotateAnimation.setFillAfter(true);
-        iv_marrow.setAnimation(rotateAnimation);
-        currentDegree=-degree;
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+            mGravity = event.values;
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+            mGeomagnetic = event.values;
+        if (mGravity != null && mGeomagnetic != null) {
+            float R[] = new float[9];
+            float I[] = new float[9];
+            if (SensorManager.getRotationMatrix(R, I,
+                    mGravity, mGeomagnetic)) {
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R, orientation);
+                float degree = (float) Math.toDegrees(orientation[0]);
+
+                tv_mdegree.setText(Float.toString(degree));
+
+            }
+        }
     }
+
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
